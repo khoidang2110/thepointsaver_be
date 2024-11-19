704ae7e321ef605424209419010b990a55aaadee
“khoidang2110” <khoidang2110@gmail.com>
2024-11-19 18:20:30 +0700

    up2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'thepointsaver be!';
+    return 'thepointsaver be! 19/11';
   }
 }
