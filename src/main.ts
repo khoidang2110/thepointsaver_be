877ad699779b5e034e64b6d432e4adf2ad9d13c6
“khoidang2110” <khoidang2110@gmail.com>
2024-11-22 11:52:36 +0700

    cors config

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,26 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './filters/all-exceptions.filter';
-import * as cors from 'cors';
-async function bootstrap() {
 
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
 
-    // // CORS
-    // const allowedOrigins = [
-    //   'https://thepointsaver.vercel.app/',
-    //   'http://localhost:3000',
-    
-    // ];
-  
-    // const corsOptions = {
-    //   origin: (origin, callback) => {
-    //     if (allowedOrigins.includes(origin) || !origin) {
-    //       callback(null, true);
-    //     } else {
-    //       callback(new Error('Not allowed by CORS'));
-    //     }
-    //   },
-    //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    //   allowedHeaders: ['Content-Type', 'Authorization'],
-    //   credentials: true,
-    // };
-  
-    // app.use(cors(corsOptions)); // Sử dụng CORS với cấu hình
+    .setTitle('The point saver API')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
 
+  const swagger = SwaggerModule.createDocument(app, config);
 
-const config  = new DocumentBuilder()
+  SwaggerModule.setup('swagger', app, swagger);
 
-.setTitle("The point saver API")
-.addBearerAuth()
-.setVersion("1.0")
-.build()
-
-//  // Sử dụng ValidationPipe để tự động validate DTO
-//  app.useGlobalPipes(new ValidationPipe());
-
-//  // Sử dụng bộ lọc ngoại lệ mặc định
-//  const { httpAdapter } = app.get(HttpAdapterHost);
-//  app.useGlobalFilters(new AllExceptionsFilter(httpAdapte));
-
-const swagger = SwaggerModule.createDocument(app,config);
-
-
-SwaggerModule.setup("swagger",app,swagger)
-
-
-
-
-
-
-app.enableCors();
+  app.enableCors({
+    origin: ['https://thepointsaver.vercel.app/', 'http://localhost:3000'], // Địa chỉ client frontend của bạn
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization', // Chắc chắn cho phép Authorization header
+  });
   await app.listen(8083);
 }
 bootstrap();
-
